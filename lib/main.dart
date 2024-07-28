09d3f76e30e74176ec5c5d756ac800a696077865
okatoku331 <okatoku331@gmail.com>
2024-07-28 22:07:59 +0900

    6.1.42: 31リリース

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       title: '天運三柱推命',
       theme: ThemeData.dark(),
       home: const MyHomePage(
-        apptitle:'天運三柱推命 ver6.1.41', // Todo: 修正したらバージョンをあげる
+        apptitle:'天運三柱推命 ver6.1.44', // Todo: 修正したらバージョンをあげる
         seinenInt: 2000, // 6.1.2
         seigatuInt: 1, // 6.1.2
         seinitiInt: 1, // 6.1.2
